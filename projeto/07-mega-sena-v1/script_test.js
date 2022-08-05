61d21fb1360e9b424e3b5da036589521673a30a0
João Antônio <66127911+joaocrz@users.noreply.github.com>
2022-08-05 16:58:49 -0300

    Add files via upload

---
+++ projeto/07-mega-sena-v1/script_test.js
@@ -0,0 +1,87 @@
+var state = { board: [], currentGame: [], savedGames: [] };
+
+function start() {
+  addNumberToGame(1);
+  addNumberToGame(2);
+  addNumberToGame(3);
+  addNumberToGame(4);
+  addNumberToGame(5);
+  saveGame();
+  addNumberToGame(6);
+
+  saveGame();
+  saveGame();
+
+  console.log(state.currentGame);
+  console.log(state.savedGames);
+
+  resetGame();
+  console.log(state.currentGame);
+}
+
+function addNumberToGame(numberToAdd) {
+  if (numberToAdd < 1 || numberToAdd > 60) {
+    console.error('Número inválido', numberToAdd);
+    return;
+  }
+
+  if (state.currentGame.length >= 6) {
+    console.error('O jogo já está completo.');
+    return;
+  }
+
+  if (isNumberInGame(numberToAdd)) {
+    console.error('Este número já está no jogo.', numberToAdd);
+    return;
+  }
+
+  state.currentGame.push(numberToAdd);
+}
+
+function removeNumberFromGame(numberToRemove) {
+  if (numberToRemove < 1 || numberToRemove > 60) {
+    console.error('Número inválido', numberToRemove);
+    return;
+  }
+  var newGame = [];
+
+  for (var i = 0; i < state.currentGame.length; i++) {
+    var currentNumber = state.currentGame[i];
+
+    if (currentNumber === numberToRemove) {
+      continue;
+    }
+
+    newGame.push(currentNumber);
+  }
+
+  state.currentGame = newGame;
+}
+
+function isNumberInGame(numberToCheck) {
+  //if (state.currentGame.includes(numberToCheck)) {
+  //  return true;
+  //}
+  //return false;
+
+  return state.currentGame.includes(numberToCheck);
+}
+
+function saveGame() {
+  if (!isGameComplete()) {
+    console.error('O jogo não está completo');
+    return;
+  }
+
+  state.savedGames.push(state.currentGame);
+}
+
+function isGameComplete() {
+  return state.currentGame.length === 6;
+}
+
+function resetGame() {
+  state.currentGame = [];
+}
+
+start();
